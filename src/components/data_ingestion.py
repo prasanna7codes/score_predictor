eb245baa28c75eaddba4bce5d1c32391d7446415
prasanna7codes <prasannalit098@gmail.com>
2024-12-28 15:42:48 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,21 +7,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-'''from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-'''
 
 '''from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 '''
 @dataclass
-class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")#this is where the output of my dataingestion will be stores
+class DataIngestionConfig: #defines configuration i.e where the output will be stored
+    train_data_path: str=os.path.join('artifacts',"train.csv") 
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
-class DataIngestion:
-    def __init__(self):
+class DataIngestion: #handles data ingestion tasks.
+    def __init__(self): #Initializes the class and creates a DataIngestionConfig object.
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
@@ -53,7 +52,11 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
     
 
